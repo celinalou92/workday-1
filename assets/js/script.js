24eccc85847dc3a60b5b56b35b001fab5ce05ffc
Celina Louissaint <celinalouissaint@gmail.com>
2021-03-14 12:30:02 -0700

    hour classes and logic added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+let taskList = JSON.parse(localStorage.getItem('taskList')) || [];
+let retrievedTaskList = JSON.parse(localStorage.getItem("taskList", taskList));
+
 $("document").ready(function(){
     // display current day and time in p with #currentDay
         $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"))
@@ -9,7 +12,7 @@ $("document").ready(function(){
     
     // select col and add class based on the time
     $(".task-block").each(function(){
-        let timeId = $(this).attr("id")
+        let timeId = $(this).parent().attr("id")
         console.log(timeId)
         if(timeId > currentTime){
             // add class .future for future hours
@@ -26,24 +29,41 @@ $("document").ready(function(){
     // Question: it doesn't seem like moment returns a number? does it return a string? === does not work .... can I parseInt??
        
     // click function to add tasks
-    // $(".task-block").on("click", function(){
-    //     console.log("clicked")
-    //     //  create element to store text
-    //     let text = $(this).text().trim()
-    //     // time blocked clicked needs to add a child element text area
+    $(".hour-row").on("click", ".saveBtn", function(){
+        console.log("clicked")
+        let hourId = $(this).parent().attr("id")
+        console.log(hourId)
+        //  create element to store text
+        let textInput = $(this).prev(".task-block").val().trim();
+        console.log(textInput)
+        
+        // store both values in a object
+        let taskOb = {
+            hour: hourId,
+            task: textInput
+        }
+        // push object into the array
+        taskList.push(taskOb)
+        console.log(taskList)
+        localStorage.setItem("taskList", JSON.stringify(taskList));
+        // take in that input and save it to localStorage
+
+    });
+        
+    $(".hour-row").each(function(){
+        for(let i = 0; i < retrievedTaskList.length; i++){
+            if($(this).attr("id") == retrievedTaskList[i].hour){
+                let taskText = $(this).prev(".task-block").val() 
+                taskText = retrievedTaskList[i].task
+                $(".hour-row").child(".task-block").append(taskText)
+                // console.log(retrievedTaskList[i].hour)
+                // console.log($(this).attr("id"))
+                // console.log(retrievedTaskList[i].task)
+            }
+        }
         
         
-    //     console.log(text)
-    //     console.log(textInput)
-       
-    //     // take in that input and save it to localStorage
-    //     // on blur takes input and makes it into a <p> element 
-    //     $("span").replaceWith(textInput);
-    //     textInput.trigger("focus");
-    //     // exit edit when out of focus
-    // })
-        
-    
-    
+    })
+    console.log(retrievedTaskList)
 })
 
